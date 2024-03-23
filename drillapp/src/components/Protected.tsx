0619fe4b36bc4df0c6045ce752aa7707351cd2d6
Shighetari <shighetari@gmail.com>
2024-03-23 18:28:31 -0400

    added some basic dynamic values to pull from keycloak instance for testing purposes. Smooth sailing so far.

--- drillapp/src/components/Protected.tsx
+++ drillapp/src/components/Protected.tsx
@@ -1,11 +1,29 @@
+import { useKeycloak } from '@react-keycloak/web';
 
 function Protected() {
+  const { keycloak } = useKeycloak();
+
+  const userDetails = {
+    username: keycloak.tokenParsed?.preferred_username,
+    email: keycloak.tokenParsed?.email,
+    fullName: keycloak.tokenParsed?.name,
+    firstName: keycloak.tokenParsed?.given_name,
+    lastName: keycloak.tokenParsed?.family_name,
+    roles: keycloak.realmAccess?.roles.join(', '),
+  };
 
   return (
-    <>
-     <p> Protected </p>
-    </>
-  )
+    <div>
+      <h2>Protected Page</h2>
+      <ul>
+        {Object.entries(userDetails).map(([key, value]) => (
+          <li key={key}>
+            <strong>{key.charAt(0).toUpperCase() + key.slice(1)}:</strong> {value || 'Not Available'}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default Protected
+export default Protected;
--- drillapp/src/components/Public.tsx
+++ drillapp/src/components/Public.tsx
@@ -1,11 +1,19 @@
+// Import useKeycloak hook
+import { useKeycloak } from '@react-keycloak/web';
 
 function Public() {
+  const { keycloak } = useKeycloak();
+
+  const handleLogin = () => {
+    keycloak.login();
+  };
 
   return (
     <>
-<p> Public </p>
+      <p>Public Page</p>
+       <button onClick={handleLogin}>Login</button>
     </>
-  )
+  );
 }
 
-export default Public
+export default Public;
